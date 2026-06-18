168708bff3189b10b1b75a921dcc187ff585132f
Aleksander Kurgan <113726847+Rand0m0User1@users.noreply.github.com>
2026-06-18 04:07:09 -0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = zip(position, speed)
+        pairs_sorted = sorted(pairs, reverse=True)
+        fleets = 0
+        max_time = 0.0
+
+        for i in range(len(pairs_sorted)):
+            time_to_finish = (target - pairs_sorted[i][0]) / pairs_sorted[i][1]
+            # distance/vel = time
+
+            if time_to_finish > max_time:
+                fleets += 1
+                max_time = time_to_finish
+
+        return fleets
